1a0a4ef7c527c9f31163bc7c4dec5c2cc9fa0cfb
Masaki Komagata <komagata@gmail.com>
2014-11-12 17:30:24 +0900

    Add pg commander

---
+++ Casks/pgcommander.rb
@@ -0,0 +1,8 @@
+class PGCommander < Cask
+  url       'https://eggerapps.at/pgcommander/download/'
+  homepage  'https://eggerapps.at/pgcommander/'
+  version   'latest'
+  app       'PG Commander.app'
+  uninstall 'PG Commander.app'
+  sha256    :no_check
+end
