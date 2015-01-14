77ca8c215c76bbc1869376d81d628809b81c89c6
Masaki Komagata <komagata@gmail.com>
2015-01-14 17:05:53 +0900

    Update brew-mycask.rb

--- brew-mycask.rb
+++ brew-mycask.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class BrewMycask < Formula
-  homepage 'https://github.com/komagata/homebrew-mycask/'
-  url 'https://github.com/komagata/homebrew-mycask.git'
+  homepage 'https://github.com/komagata/mycask/'
+  url 'https://github.com/komagata/mycask.git'
   def install
   end
 end
